2eebcbaa3f9c5b4316f9c2d229cea39877191f96
Jacob Doherty <doherty.jacob.p@gmail.com>
2022-09-09 15:50:39 -0700

    add answers variable with ansers to JS

--- app.js
+++ app.js
@@ -6,6 +6,28 @@ const fortuneContainer = document.getElementById('fortune-container');
 
 const fortuneOutput = document.getElementById('fortune-output');
 
+const answers = [
+    'Yes, definitely',
+    'It is certain',
+    'It is decidedly so',
+    'Without a doubt',
+    'You may rely on it',
+    'As I see it, yes',
+    'Most Likely',
+    'Outlook good',
+    'Signs point to yes',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Better not tell you now',
+    'Cannot predict now',
+    'Concentrate and ask again',
+    'Don’t count on it',
+    'My reply is no',
+    'My sources say no',
+    'Outlook not so good',
+    'Very doubtful',
+];
+
 askButton.addEventListener('click', () => {
     questionContainer.classList.toggle('hide');
     fortuneContainer.classList.toggle('hide');
